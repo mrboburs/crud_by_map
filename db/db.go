e21224b7b164a665342ca8b3a46f604254508524
mrboburs <babdusalom72@gmail.com>
2022-03-12 23:15:37 +0500

    commit

--- db/db.go
+++ db/db.go
@@ -1,21 +1,89 @@
 package db
 
 import (
-// "crudbymap/model"
-// "fmt"
+	"crudbymap/model"
+	"errors"
+	// "fmt"
 )
 
-// func g(){
-// 	myMap:= map([string]model.Article)
-// 	fmt.Println(myMap)
-// }
+// type articleStorage map[int]models.Article
+func NewArticleStorage() articleStorage {
+	return articleStorage{
+		data: map[int]model.Article{},
+	}
 
-// myMap["item"]
-// myMap = map[string]model.Article{
-// "item1": model.Article{
-// 	ID:        1,
-// 	Content:   model.Content{Title: "heading", Body: "main"},
-// 	Author:    model.Person{Firstname: "bob", Lastname: "Mr"},
-// 	CreatedAt: &time.Time{},
-// },
-// }
+}
+
+type articleStorage struct {
+	data map[int]model.Article
+}
+
+//Errors varableName
+var ErrorAlreadyExists = errors.New("already exists id")
+var ErrorNotFound = errors.New("not found id ")
+var ErrorNotUpdated = errors.New("not update because not found id")
+var ErrorSearch = errors.New("not found data")
+
+//Successes varableName
+var SuccessUpdated = errors.New("SuccessFull Updated")
+var SuccessAdded = errors.New("SuccessFull Added")
+var SuccessDeleted = errors.New("SuccessFull Deleted")
+var SuccessGetByID = errors.New("SuccessFull Got by ID")
+
+//add new article
+func (storage *articleStorage) Add(entity model.Article) error {
+	if _, ok := storage.data[entity.ID]; ok {
+		// fmt.Println(ok, 2, x)
+		return ErrorAlreadyExists
+	}
+	storage.data[entity.ID] = entity
+	return SuccessAdded
+}
+
+//get article by id return map
+func (storage *articleStorage) GetByID(ID int) (model.Article, error) {
+	var resp model.Article
+	resp, ok := storage.data[ID]
+	if ok {
+		return resp, SuccessGetByID
+	}
+	return resp, ErrorNotFound
+}
+
+// get all article return array
+func (storage *articleStorage) GetList() []model.Article {
+	var resp []model.Article
+	for _, data := range storage.data {
+		resp = append(resp, data)
+	}
+	return resp
+}
+
+//search article via title return array
+func (storage *articleStorage) Search(str string) []model.Article {
+	var resp []model.Article
+	for _, data := range storage.data {
+		if data.Content.Title == str {
+			resp = append(resp, data)
+		}
+	}
+	return resp
+}
+
+//update exist article
+func (storage *articleStorage) Update(entity model.Article) error {
+	if _, ok := storage.data[entity.ID]; ok {
+		storage.data[entity.ID] = entity
+		return SuccessUpdated
+	}
+	return ErrorNotUpdated
+}
+
+//delete article via id
+func (storage *articleStorage) Delete(ID int) error {
+	if _, ok := storage.data[ID]; ok {
+		delete(storage.data, ID)
+		return SuccessDeleted
+	}
+	return ErrorNotFound
+}
--- main.go
+++ main.go
@@ -1,75 +1,120 @@
 package main
 
 import (
-	// "crudbymap/cmd/model"
+	"crudbymap/db"
 	"crudbymap/model"
 	"fmt"
 	"time"
+	// "github.com/gin-gonic/gin"
 )
 
-var myMap = map[string]model.Article{}
+// var inMemory storage.ArticleStorage
 
-func getAll() {
-	fmt.Println(myMap)
-}
-func getById(id int) {
-	for _, val := range myMap {
-		if val.ID == id {
-
-			// fmt.Println(val)
-			fmt.Println(val)
-		}
-	}
-}
-
-func Delete(id int) {
-	for key, val := range myMap {
-		if val.ID == id {
-			val.Content.Body = "updatedBody"
-			delete(myMap, key)
-			// fmt.Println(val)
-			fmt.Println(myMap)
-		}
-	}
-}
-func Update(id int) {
-	for _, val := range myMap {
-		if val.ID == id {
-			val.Content.Body = "updatedBody"
-			fmt.Println(val)
-			// fmt.Println(myMap)
-		}
-	}
-}
-
-func Create() {
-
-	myMap["item1"] = model.Article{
-
-		ID:        1,
-		Content:   model.Content{Title: "heading", Body: "main"},
-		Author:    model.Person{Firstname: "bob", Lastname: "Mr"},
-		CreatedAt: &time.Time{},
-	}
-	myMap["item2"] = model.Article{
-		ID:        2,
-		Content:   model.Content{Title: "heading2", Body: "main2"},
-		Author:    model.Person{Firstname: "bob2", Lastname: "Mr2"},
-		CreatedAt: &time.Time{},
-	}
-
-	myMap["item3"] = model.Article{
-		ID:        3,
-		Content:   model.Content{Title: "heading3", Body: "main3"},
-		Author:    model.Person{Firstname: "bob3", Lastname: "Mr3"},
-		CreatedAt: &time.Time{},
-	}
-	// fmt.Println(myMap)
-
-}
 func main() {
 
-	Create()
-	// Update(1)
-	Delete(1)
+	// articleStorage = make(storage.ArticleStorage)
+	articleStorage := db.NewArticleStorage()
+
+	//add new article
+	// r := gin.Default()
+	//1 article
+	var a1 model.Article
+	a1.ID = 1
+	a1.Content.Title = "Lorem"
+	a1.Content.Body = "Lorem ipsum"
+	var a1p model.Person = model.Person{
+		Firstname: "John",
+		Lastname:  "Doe",
+	}
+	a1.Author = a1p
+	t := time.Now()
+	a1.CreatedAt = &t
+	// err := articleStorage.Add(a1)
+	// if err != nil {
+	// 	fmt.Println(err, 1)
+	// }
+	// r.POST("/add", articleStorage.Add(a1))
+
+	//2 - article
+	var a2 model.Article
+	a2.ID = 2
+	a2.Content.Title = "Hello Nova"
+	a2.Content.Body = "NASA"
+	var a2p model.Person = model.Person{
+		Firstname: "John",
+		Lastname:  "Carter",
+	}
+	a2.Author = a2p
+	a2.CreatedAt = &t
+	// err = articleStorage.Add(a2)
+	// if err != nil {
+	// 	fmt.Println(err, 2)
+	// }
+
+	//3 article
+	var a3 model.Article
+	a3.ID = 3
+	a3.Content.Title = "Why 13 Reasons"
+	a3.Content.Body = "Cinema"
+	var a3p model.Person = model.Person{
+		Firstname: "Hanna",
+		Lastname:  "Backer",
+	}
+	a3.Author = a3p
+	a3.CreatedAt = &t
+	// err = articleStorage.Add(a3)
+	// if err != nil {
+	// 	fmt.Println(err, 3)
+	// }
+	// err = articleStorage.Add(a1)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	//update 1 article update
+	var updateA1 model.Article
+	updateA1.ID = 1
+	updateA1.Content.Title = "Go"
+	updateA1.Content.Body = "Golang"
+	var updateA1Person model.Person = model.Person{
+		Firstname: "MRB",
+		Lastname:  "Hero",
+	}
+	updateA1.Author = updateA1Person
+
+	//delete article  by id
+	// deleteArticle := articleStorage.Delete(2)
+
+	//Update article by ID
+	// updateArticle := articleStorage.Update(updateA1)
+
+	//Search  Article via Article Title
+	searchArticleList := articleStorage.Search("Why 13 Reasons")
+	if len(searchArticleList) == 0 {
+		fmt.Println(db.ErrorSearch)
+	}
+	//get all article list
+	// articleList := articleStorage.GetList()
+
+	//get by id
+	// getArticleByID, err := articleStorage.GetByID(1)
+	// if err != nil {
+	// 	fmt.Print(err)
+	// }
+
+	// fmt.Println()
+	// fmt.Println("Get List    : ", articleList)
+	// fmt.Println()
+	// fmt.Println("Create storage: ", articleStorage)
+	// fmt.Println()
+	// fmt.Println("Get by ID: ", getArticleByID)
+	// fmt.Println()
+	// fmt.Println("Delete by ID: ", deleteArticle)
+	// fmt.Println()
+	// fmt.Println("Update by ID: ", updateArticle)
+	// fmt.Println()
+	// fmt.Println("Search by string:  ", searchArticleList)
+	// fmt.Println()
+	// fmt.Println("Get List    : ", articleList)
+	// fmt.Println()
 }
